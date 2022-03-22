cb3d7d852f74a8412f8ff589b6414728ed072d08
Manthan Upadhyaya <manthanand@gmail.com>
2022-03-21 19:02:59 -0500

    Fixed frequency measurements based on doubled frequency of rectifier

--- SeniorDesign.ino
+++ SeniorDesign.ino
@@ -10,15 +10,15 @@ void loop() {
   int count = 0;
   //read time before counting
   oldTime = millis();
-  while (count != 60){
-    //Analog Read reads values from 0-1023; 0-15 is between 0-.05V
-    while(analogRead(A2) > 15); //wait for analog Read to read close to 0 volts
+  while (count != 120){
+    //Analog Read reads values from 0-1023
+    while(analogRead(A2) > 5); //wait for analog Read to read close to 0 volts
     count++; //need to make sure that 15 isn't too large (i.e if the bounce stays within .05V the next time analog Read occurs, need to decrease it)
     //also need to make sure that analogRead can be run in quick succession without reading bad values
   }
 
   //As time increases, frequency decreases
-  float Hz = 60000/(millis() - oldTime); //time it took to get to 60 0 crossings
+  float Hz = 120000/(millis() - oldTime); //time it took to get to 120 0 crossings
   //accuracy up to 1/1000 hz hopefully
   Serial.println(Hz);
 }
