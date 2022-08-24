7e3cd99cae0f5a2ae31fecc6a73f79d6bd6a5569
Manthan Upadhyaya <manthanand@DESKTOP-GKML8N1.localdomain>
2022-08-24 14:16:35 -0500

    Finished prototype software architecture

--- ML.py
+++ ML.py
@@ -1,7 +1,41 @@
-import tensorflow
+# This library is in charge of collecting data, training the ML algorithm, and making predictions
 
-def collect_data():
+import tensorflow
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import os
+import time
+
+WEATHER_DATA = None
+SUPPLY_DATA = None
+DEMAND_DATA = None
+
+# This function will collect weather data and store it in "WeatherCSV.csv". 
+# This data will then be used to predict supply and demand
+def collect_data_weather():
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Austin%2C%20TX?unitGroup=us&include=hours&key=5BSYNGCVWT67XMUAJSFWRV4LT&contentType=csv"
+    weathercsv = "WeatherCSV.csv"
+    text = "WeatherText.txt"
+
+    while(True):
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'html.parser')
+        with open(text, 'w') as myFile:
+            myFile.write(soup.text)
+        myFile.close()
+
+        dateframe = pd.read_csv(text)
+        dateframe.to_csv(weathercsv)
+        myFile.close()
+        os.remove(text)
+        print("Generated new weather csv")
+        time.sleep(900) #NOTE: This is because we are getting data for free every 15 minutes.
+
+# This function collects the current supply and demand for all clusters and stores them in "CurrentData.csv"
+def collect_data_supplydemand():
     return
 
+# This function trains based on the collected data and writes predictions to "PredictedData.csv"
 def train():
     return 
--- Main.py
+++ Main.py
@@ -1,6 +1,10 @@
+# This program collects weather, supply, and demand data and uses that to train a Machine Learning Algorithm
+# to make accurate predictions about future supply and demand. This is used to implement a safe rolling blackout
+# sequence using a High Level State Machine.
 import ML
 import FSM
 import serial
+import threading
 
 UPPERBOUND = 60.05
 LOWERBOUND = 59.95
@@ -11,8 +15,23 @@ ser = serial.Serial(
 )
 
 if __name__ == "__main__":
-    freq = ser.read()
-    while  freq < UPPERBOUND or freq > LOWERBOUND:
-        ML.train()
+    t1 = threading.Thread(target = ML.collect_data_weather,) #Constantly collect weather data
+    t2 = threading.Thread(target = ML.collect_data_supplydemand,)
+    t3 = threading.Thread(target = ML.train())
+
+    t1.start()
+    t2.start()
+    t3.start()
+
+    while True: 
         freq = ser.read()
+        while  freq < UPPERBOUND or freq > LOWERBOUND: # This blocks until an out-of-bounds frequency is read
+            freq = ser.read()
+
+        t4 = threading.Thread(target = FSM.greedyPick()) # Start FSM thread 
+        t4.start()
+        t4.join() # If FSM thread finishes, we are in safe state. Go back to checking if blackout will happen again
     
+    t1.join() #This should never be reached
+    t2.join()
+    t3.join()
