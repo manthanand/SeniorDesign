942c1a3c6dfd49fa4851a3a6857e7026e305b6e0
evargh <varghese.evan@gmail.com>
2022-08-29 13:19:18 -0500

    implemented greedypick

--- FSM.py
+++ FSM.py
@@ -9,27 +9,33 @@ P2 = []
 P3 = []
 
 class building:
-    def __init__(self,nameItem,priorityItem,demandItem):
-        self.name = nameItem
-        self.priority = priorityItem
-        self.demand = demandItem
+    def __init__(self,name_item,priority_item,demand_item):
+        self.name = name_item
+        self.priority = priority_item
+        self.demand = demand_item
 
 with open('./SeniorDesign/buildingList.csv', newline='') as csvfile:
-    buildingReader = csv.reader(csvfile, delimiter=',')
-    for row in buildingReader:
-        rowBuilding = building(nameItem=row[0],priorityItem=row[1],demandItem=row[2])
-        match rowBuilding.priority:
+    building_reader = csv.reader(csvfile, delimiter=',')
+    for row in building_reader:
+        row_building = building(name_item=row[0],priority_item=row[1],demand_item=row[2])
+        match row_building.priority:
             case 'P1':
-                P1.append(rowBuilding)
+                P1.append(row_building)
             case 'P2':
-                P2.append(rowBuilding)
+                P2.append(row_building)
             case 'P3':
-                P3.append(rowBuilding)
+                P3.append(row_building)
             case _:
-                print("Building Omitted: " + rowBuilding.name)
+                print("Building Omitted: " + row_building.name)
 
 PRIORITIES = [P1, P2, P3]
 
-def greedyPick(projectedSupply):
+def greedy_pick():
+    # read from current_data.csv
+    # read from predicted_data.csv
+    for priority_list in PRIORITIES:
+        sorted_list = sorted(priority_list, key=lambda building: building.demand, reverse=True)
+        for item in sorted_list:
+            print(item.name + " " + item.demand)
 
 # make class structure for each building--projected demand
