f28790ac274501efc8fed1d886bb0aaf75027e9d
Manthan Upadhyaya <manthanand@DESKTOP-GKML8N1.localdomain>
2022-09-12 14:13:17 -0500

    Allowed floating point math

--- SeniorDesign.ino
+++ SeniorDesign.ino
@@ -1,7 +1,5 @@
-#include <ESP32TimerInterrupt.h>
 
 void setup() {
-  pinMode(LED, OUTPUT);     // Defining pin as output
   Serial.begin(115200);       // Initiating Serial communication
 }               
                                                                                                                                                                                                             
@@ -13,17 +11,17 @@ void loop() {
   while (count != 120){
     //Analog Read reads values from 0-1023
     while(analogRead(A2) > 5); //wait for analog Read to read close to 0 volts
-    if ((millis() - oldTime) > 1500){ //wait 1.5 seconds before erroring out
-      Serial.println("Error");
-      break
-    }
-    delay(3) //wait 3ms before checking for 0's again
+//    if ((millis() - oldTime) > 1500){ //wait 1.5 seconds before erroring out
+//      Serial.println("Error");
+//      break
+//    }
+//    delay(3); //wait 3ms before checking for 0's again
     count++; //need to make sure that 15 isn't too large (i.e if the bounce stays within .05V the next time analog Read occurs, need to decrease it)
     //also need to make sure that analogRead can be run in quick succession without reading bad values
   }
 
   //As time increases, frequency decreases
-  float Hz = 120000/(millis() - oldTime); //time it took to get to 120 0 crossings
+  float Hz = 120000.0/(float)(millis() - oldTime); //time it took to get to 120 0 crossings
   //accuracy up to 1/1000 hz hopefully
   Serial.println(Hz);
 }
