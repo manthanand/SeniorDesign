d66ea98ff99490f2c61b2d85299150effa21187f
Manthan Upadhyaya <manthanand@DESKTOP-GKML8N1.localdomain>
2022-09-21 22:48:51 -0500

    Added priority to csv

--- TheDude/ML.py
+++ TheDude/ML.py
@@ -10,12 +10,7 @@ import supply_ml
 
 # This is a dictionary where the key is the cluster name and the value is the csv that
 # contains the data associated with that key
-cluster_csv = {}
-
-# This function creates a dictionary that will be used when training the data
-def init():
-    clusters = pd.read_csv(settings.clusterfp).to_dict('records')
-    for i in clusters: cluster_csv[i['Cluster']] = settings.demandfp + i['CSV']
+clusters = pd.read_csv(settings.clusterfp)
 
 # This function collects the current supply and demand for all clusters and stores them in "OutputData.csv" every 15 minutes
 # It uses input data from the folder InputData
@@ -28,26 +23,28 @@ def train():
     
     # THIS WILL COLLECT WEATHER DATA AND STORE IT IN "WeatherCSV.csv"
     url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Austin%2C%20TX?unitGroup=us&include=hours&key=5BSYNGCVWT67XMUAJSFWRV4LT&contentType=csv"
-    weathercsv = "WeatherCSV.csv"
+    weathercsv = "TheDude/WeatherCSV.csv"
     text = "WeatherText.txt"
 
-    html = requests.get(url).content
-    soup = BeautifulSoup(html, 'html.parser')
-    with open(text, 'w') as myFile:
-        myFile.write(soup.text)
-    myFile.close()
-
-    weather_df = pd.read_csv(text)
-    weather_df.to_csv(weathercsv)
-    myFile.close()
-    os.remove(text)
-    print("Generated new weather csv")
+    try: 
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'html.parser')
+        with open(text, 'w') as myFile:
+            myFile.write(soup.text)
+        myFile.close()
+        weather_df = pd.read_csv(text)
+        weather_df.to_csv(weathercsv)
+        myFile.close()
+        os.remove(text)
+        print("Generated new weather csv")
+    except: weather_df = pd.read_csv(weathercsv) #use old one if cant read from url
 
     # Write Supply data to dataframe
     output.loc[len(output.index)] = (["Supply", "", "", "", "", "", ""] + supply_ml.generate_supply_predictions(weather_df))
     # Write Demand data to dataframe
-    for i in cluster_csv: output.loc[len(output.index)] = ([i] + demand_ml.generate_demand_predictions(cluster_csv[i]) + ["", "", "", "", "", ""])
+    print(clusters.iterrows())
+    for i, r in clusters.iterrows(): 
+        output.loc[len(output.index)] = ([r["Cluster"]] + [r["Priority"]] + demand_ml.generate_demand_predictions(settings.demandfp + r["CSV"]) + ["", "", "", "", ""])
     output.to_csv(settings.outputfp, encoding='utf-8', index=False) # Write Dataframe to csv
 
-init()
 train()
