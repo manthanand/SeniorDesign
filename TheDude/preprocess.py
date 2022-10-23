7e9ea22f396c2713122e6e40310470b4eb8d8ff8
Manthan Upadhyaya <manthanand@gmail.com>
2022-10-23 17:53:44 -0500

    Removed CSV's with no data in them

--- TheDude/preprocess.py
+++ TheDude/preprocess.py
@@ -29,13 +29,27 @@ def add_priorities():
 def replace_rows():
     for i, r in clusters.iterrows():
         fp = glob.glob(settings.demandfp + r["Cluster"] + "*")[0]
+        print(fp)
         cluster_data = pd.read_csv(fp, header=0, index_col=0)
         demand = cluster_data["value"].tolist()
-        for i, e in reversed(list(enumerate(demand))):
-            if ((i >= 1) and ((demand[i] - demand[i - 1]) >= 0)): demand[i] -= demand[i - 1]
+        prev = 1 #flag for whether previous element was 0
+        for i, e in reversed(list(enumerate(demand))): #traverse through row from end
+            if (i >= 1): 
+                if (demand[i - 1] < 0): #if next value is negative, don't subtract
+                    pass
+                elif ((demand[i] - demand[i - 1]) > 0): 
+                    demand[i] -= demand[i - 1]
+                    prev = 1
+                elif ((demand[i] - demand[i - 1]) == 0): #if current - previous = 0
+                    if (prev == 0): #if this is second time it was 0, set all to -1
+                        demand[i:i+3] = [-1,-1,-1]
+                    else: #else let it be 0
+                        demand[i] -= demand[i - 1]
+                        prev = 0
+                else: #if negative, keep demand[i] the same
+                    pass
         cluster_data["value"] = demand
-        cluster_data.to_csv(fp)
-
+        cluster_data.to_csv(fp)           
 
 if __name__ == "__main__":
     replace_rows()
