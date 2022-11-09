aa55ba065ff1a46ed8071e613db95df522b25dc9
Neeley Pate <60448247+neeley-pate@users.noreply.github.com>
2022-11-09 14:56:15 -0600

    for evan

--- TheDude/supply_ml.py
+++ TheDude/supply_ml.py
@@ -10,6 +10,11 @@ from datetime import datetime
 from tensorflow import math
 from tensorflow import reduce_mean
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from keras import Sequential
+from keras.layers import Dense
+from keras.layers import BatchNormalization
 import settings
 import glob
 import sys
@@ -30,7 +35,6 @@ COUNTER = 0
 PREVIOUS_PREDICTION = 42069
 PREDICTION_SET_SIZE = 580
 N_TEST = 50
-PREDICTION_SET_SIZE = 580
 # Dictionary key is cluster model path, value is list with [prediction accuracy, counter]
 
 # Should be used by layer above to increment amount of time horizons that ML has predicted
@@ -66,7 +70,7 @@ def custom_loss(y_actual, y_pred):
 
     Zeros = tf.zeros_like(MSE)  # create tensor of zeros
     Mask = [False, False, False, False, False, False, False, False, False, False,
-            False, False, False, False, False, False, False, False, False, False, True]  # create mask
+            False, False, False, False, False, False, False, True]  # create mask
     Solar_MSE = tf.where(Mask, MSE, Zeros)  # create tensor where every loss is 0 except solar output
 
     # print_output = tf.print(Solar_MSE, "Solar_MSE: ")
@@ -80,7 +84,7 @@ def custom_eval(y_actual, y_pred):
 
     Zeros = tf.zeros_like(MSE)  # create tensor of zeros
     Mask = [False, False, False, False, False, False, False, False, False, False,
-            False, False, False, False, False, False, False, False, False, False, True]  # create mask
+            False, False, False, False, False, False, False, True]  # create mask
     Solar_MSE = tf.where(Mask, MSE, Zeros)  # create tensor where every loss is 0 except solar output
 
     Solar_RMSE = tf.math.sqrt(Solar_MSE)
@@ -90,36 +94,62 @@ def custom_eval(y_actual, y_pred):
     return Solar_RMSE
 
 def generate_model(starting, model_location, csv):
+    '''df = pd.read_csv(csv, index_col=0)
+    df = df.head(n=starting)
+    column_mean = []
+    column_std = []
+    pastWeatherAndSupply = df.iloc[:-2].drop(['year', 'day'], axis=1)
+
+    currentWeather = df.iloc[-2]
+    currentWeather = currentWeather.drop(['Percent Output', 'year', 'day'])  # dropping supply column for testing
+    currentWeather = currentWeather.values[:].reshape((1, len(df.iloc[-2]) - 3))
+
+    nextHourWeather = df.iloc[-1]
+    nextHourWeather = nextHourWeather.drop(['Percent Output', 'year', 'day'])  # dropping supply column for testing
+    nextHourWeather = nextHourWeather.values[:].reshape((1, len(df.iloc[-1]) - 3))
+    X, y = pastWeatherAndSupply.values[:, :-1], pastWeatherAndSupply.values[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    n_features = X_train.shape[1]
+    print(n_features)
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=(n_features,)))
+    model.add(Dense(12, activation='tanh', kernel_initializer='he_normal'))
+    model.add(Dense(8, activation='selu', kernel_initializer='he_normal'))
+    model.add(Dense(4, activation='relu', kernel_initializer='he_normal'))
+    model.add(Dense(1, activation='tanh', kernel_initializer='he_normal'))
+    model.compile(optimizer='adam', loss='mse')
+    model.fit(X_train, y_train, epochs=80, batch_size=10)
+    model.save(model_location)'''
     df = read_csv(csv, index_col=0)
     df = df.head(n=starting)
-    df_holdout = df.iloc[-PREDICTION_SET_SIZE:]
-    df = df.iloc[:-PREDICTION_SET_SIZE]
+    # df_holdout = df.iloc[-PREDICTION_SET_SIZE:]
 
-    holdout_X, holdout_y = split_sequence(df_holdout.values.astype('float32'), N_STEPS)
+    # holdout_X, holdout_y = split_sequence(df_holdout.values.astype('float32'), N_STEPS)
     # retrieve the values
-    # values = df.values.astype('float32')
+    values = df.values.astype('float32')
     # # specify the window size
     # n_steps = 5
     # split into samples
-    # X, y = split_sequence(values, N_STEPS)
+    X, y = split_sequence(values, N_STEPS)
     # reshape into [samples, timesteps, features]
-    holdout_X = holdout_X.reshape((holdout_X.shape[0], holdout_X.shape[1], 21))
+    # holdout_X = holdout_X.reshape((holdout_X.shape[0], holdout_X.shape[1], 21))
     # split into train/test
-    print(holdout_X[:-N_TEST])
-    X_train, X_test, y_train, y_test = holdout_X[:-N_TEST], holdout_X[-N_TEST:], holdout_y[:-N_TEST], holdout_y[-N_TEST:]
+    # print(holdout_X[:-N_TEST])
+    X_train, X_test, y_train, y_test = X[:-N_TEST], X[-N_TEST:], y[:-N_TEST], y[-N_TEST:]
     # %%
     # define model
     # improvement area : try adding dropout
     model = Sequential()
-    model.add(LSTM(100, activation='relu', kernel_initializer='he_normal', input_shape=(N_STEPS, 21)))
+    model.add(LSTM(100, activation='relu', kernel_initializer='he_normal', input_shape=(N_STEPS, 18)))
     model.add(Dense(50, activation='relu', kernel_initializer='he_normal'))
     model.add(Dense(50, activation='relu', kernel_initializer='he_normal'))
     model.add(Dense(1))
     # compile the model
-    model.compile(optimizer='adam', loss=custom_loss, metrics=[custom_eval])
-    # model.compile(optimizer='adam', loss='mse', metrics=[custom_eval])
+    # model.compile(optimizer='adam', loss=custom_loss, metrics=[custom_eval])
+    model.compile(optimizer='adam', loss='mse')
     # fit the model
-    model.fit(X_train, y_train, epochs=NUM_EPOCHS, batch_size=32, verbose=2, validation_data=(X_test, y_test))
+    model.fit(X_train, y_train, epochs=NUM_EPOCHS, batch_size=32, verbose=2, validation_data=(X_test, y_test), callbacks=tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3))
     model.save(model_location)
 
 def fit_model(model, df, points, model_location, n_tests):
@@ -140,6 +170,7 @@ def fit_model(model, df, points, model_location, n_tests):
 
 
 def compute_prediction(model_location, df):
+
     # global PREVIOUS_PREDICTION
     # # values = (df.loc[:,'value'].values).tolist()
     current = df.tail(1)
@@ -147,15 +178,18 @@ def compute_prediction(model_location, df):
     th = []
     # current_amount = wait_amount(model_location, False, True)
     # update = (current_amount == NEW_DATA_AMOUNT - 1)
-    predict_model = keras.models.load_model(model_location, custom_objects={"custom_eval": custom_eval, "custom_loss": custom_loss}, compile=False)#this is copy that will be used to make predictions
+    predict_model = keras.models.load_model(model_location, custom_objects={"custom_eval": custom_eval, "custom_loss": custom_loss})#this is copy that will be used to make predictions
     #TODO
     #This should be put into power world / Kara's thing later rather than calling .predict
     for i in range(settings.SUPPLY_TIME_HORIZONS):
         operate_current, y = split_sequence(df[-N_STEPS - 1:].values.astype('float32'), N_STEPS)
-        operate_current = operate_current.reshape((operate_current.shape[0], operate_current.shape[1], 21))
+        # operate_current = operate_current.reshape((21, operate_current.shape[1], 21))
+        # operate_current = df[-N_STEPS:].values.astype('float32')
+        # x = list()
+        # x.append(operate_current)
         prediction = predict_model.predict(operate_current, verbose=VERBOSE)
-        th.append(prediction)
-        df.append(prediction)
+        # th.append(prediction)
+        # df.append(prediction)
     # accuracy = 1
     # # Update if batch size reached or predictions become inaccurate
     # if (PREVIOUS_PREDICTION == SUPPLY_UNINIT):
@@ -173,6 +207,28 @@ def compute_prediction(model_location, df):
     # final_array = ([current] + [th[i][0][0] for i in range(settings.SUPPLY_TIME_HORIZONS)])
     return prediction
 
+'''    currentWeather = df.iloc[-2]
+    currentWeather = currentWeather.drop(['Percent Output', 'year', 'day'])  # dropping supply column for testing
+    currentWeather = currentWeather.values[:].reshape((1, len(df.iloc[-2]) - 3))
 
+    nextHourWeather = df.iloc[-1]
+    nextHourWeather = nextHourWeather.drop(['Percent Output', 'year', 'day'])  # dropping supply column for testing
+    nextHourWeather = nextHourWeather.values[:].reshape((1, len(df.iloc[-1]) - 3))
+    model = keras.models.load_model(model_location, custom_objects={"custom_eval": custom_eval, "custom_loss": custom_loss})#this is copy that will be used to make predictions
+    current_prediction = model.predict(currentWeather)
+    if (current_prediction < 0):
+        current_prediction = 0
+    print('Predicted: %.3f' % current_prediction)
+    print('Real: %.3f' % df.iloc[-2][-1])
+    print(abs(df.iloc[-2][-1] - current_prediction) / df.iloc[-2][-1])
+
+    future_prediction = model.predict(nextHourWeather)
+    if (future_prediction < 0):
+        future_prediction = 0
+    print('Predicted: %.3f' % future_prediction)
+    print('Real: %.3f' % df.iloc[-1][-1])
+    print(abs(df.iloc[-1][-1] - future_prediction) / df.iloc[-1][-1])'''
+
+# compute_prediction(settings.modelfp + "Supply", read_csv((glob.glob(settings.supplyfp)[0]), header=0, index_col=0).head(100))
 
 
